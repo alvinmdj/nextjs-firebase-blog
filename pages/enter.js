d9e86fbbf0effe02d8f842414da48ed953e61e12
AlvinMD <alvinmartindwrk@gmail.com>
2021-12-23 12:37:08 +0700

    create Loader components, setup react-hot-toast, setup Google Sign in

--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,45 @@
-export default function EnterPage () {
+import { auth, googleAuthProvider } from '../lib/firebase'
+
+export default function EnterPage (props) {
+  const user = null
+  const username = null
+
+  // 1. <SignInButton />  => if user is signed out
+  // 2. <UsernameForm />  => if user is signed in, but missing username
+  // 3. <SignOutButton /> => if user is signed in and has username
   return (
     <main>
-      <h1>Sign Up</h1>
+      {user ?
+        !username? <UsernameForm /> : <SignOutButton />
+        :
+        <SignInButton />
+      }
     </main>
   )
+}
+
+// Google Sign in
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    try {
+      await auth.signInWithPopup(googleAuthProvider)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <button className='btn-google' onClick={signInWithGoogle}>
+      <img src={'/google.png'} /> Sign in with Google
+    </button>
+  )
+}
+
+// Sign out
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>
+}
+
+function UsernameForm() {
+
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+
+import Loader from '../components/Loader'
+
+import toast from 'react-hot-toast'
 
 export default function Home() {
   return (
     <div>
-      <Link prefetch={false} href={{ 
-          pathname: '/[username]',
-          query: { username: 'alvnmd' },
-        }}>
-        <a>Alvin's profile</a>
-      </Link>
+      <button onClick={() => toast.success('hello toast')}>
+        Toast Me
+      </button>
     </div>
   )
 }
